e751988d789e743deec749c00a9a842e4513b60c
CarlyRon <carlyron@CarlyRons-MacBook-Air.local>
2025-06-03 14:05:32 -0400

    Update Terraform Cloud working directory

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -2,7 +2,7 @@ output "app_name" {
   description = "The name of the Fly.io application"
   value       = fly_app.forest_bush.name
 }
-
+#hello
 output "app_org" {
   description = "The organization the Fly.io application belongs to"
   value       = fly_app.forest_bush.org
