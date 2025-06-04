00408e6c149a36ecc6cda1bf7fe321188ad1c144
CarlyRon <carlyron@CarlyRons-MacBook-Air.local>
2025-06-03 20:39:53 -0400

    Update Terraform version and migrate to HCP Terraform

--- infra/terraform/terraform.tf
+++ infra/terraform/terraform.tf
@@ -1,16 +1,17 @@
 terraform {
-  backend "remote" {
-    organization = "Ronton"
-
-    workspaces {
-      name = "forest-bush"
-    }
-  }
-
+  required_version = "~> 1.12.0"
+  
   required_providers {
     fly = {
       source  = "fly-apps/fly"
-      version = "~> 0.0.23"
+      version = "0.0.23"
+    }
+  }
+
+  cloud {
+    organization = "Ronton"
+    workspaces {
+      name = "forest-bush"
     }
   }
 } 
