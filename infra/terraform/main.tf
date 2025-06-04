c528a937078679a4bb983a55b37cf5046897b2d7
CarlyRon <carlyron@CarlyRons-MacBook-Air.local>
2025-06-04 00:08:15 -0400

    Update Terraform Cloud should work final

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -20,23 +20,8 @@ resource "null_resource" "app_secrets" {
 
   provisioner "local-exec" {
     command = <<-EOT
-      echo "HOME is: $HOME"
-      echo "PATH is: $PATH"
-      echo "Listing $HOME:"
-      ls -al $HOME
-      echo "Searching for flyctl in $HOME:"
-      find $HOME -name flyctl
-      echo "Searching for flyctl in /usr/local/lib:"
-      find /usr/local/lib -name flyctl 2>/dev/null || true
-      echo "Which flyctl:"
-      which flyctl || true
-      echo "Command -v flyctl:"
-      command -v flyctl || true
-      echo "Trying to run flyctl version:"
-      flyctl version || true
-      # Now try the secrets commands
-      flyctl secrets set DATABASE_URL='${var.db_url}' --app ${fly_app.forest_bush.name} || true
-      flyctl secrets set REDIS_URL='${var.redis_url}' --app ${fly_app.forest_bush.name} || true
+      flyctl secrets set DATABASE_URL='${var.db_url}' --app ${fly_app.forest_bush.name}
+      flyctl secrets set REDIS_URL='${var.redis_url}' --app ${fly_app.forest_bush.name}
     EOT
   }
 
