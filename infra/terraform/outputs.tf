c847d2b4ea66eabe21451e75948974020ada7e0d
CarlyRon <carlyron@CarlyRons-MacBook-Air.local>
2025-06-04 00:23:50 -0400

    Update Terraform Cloud should work final checking redis and db

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -6,4 +6,21 @@ output "app_name" {
 output "app_org" {
   description = "The organization the Fly.io application belongs to"
   value       = fly_app.forest_bush.org
+}
+
+output "app_hostname" {
+  description = "The hostname of the Fly.io application"
+  value       = "${fly_app.forest_bush.name}.fly.dev"
+}
+
+output "database_url" {
+  description = "The PostgreSQL database URL"
+  value       = var.db_url
+  sensitive   = true
+}
+
+output "redis_url" {
+  description = "The Redis connection URL"
+  value       = var.redis_url
+  sensitive   = true
 } 
